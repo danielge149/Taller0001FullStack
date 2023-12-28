1be91bab86a1e131f11a13ed03194314695359c2
danielge149 <danielgetial149@gmail.com>
2023-12-27 20:11:35 -0500

    Subir FullStack

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mx-auto text-center">
+        <h1 class="mt-5 text-primary">Proyecto Laravel</h1>
+        
+        <div class="card mt-5">
+            <div class="card-body">
+                <h2 class="card-title">Realizado por:</h2>
+                
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        <strong>Nombre:</strong> Fredilton Daniel Getial Torres 
+                    </li>
+                    <li class="list-group-item">
+                        <strong>Código:</strong> 219036091
+                    </li>
+                    <li class="list-group-item">
+                        <strong>Celular:</strong> 3187863224
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+@endsection
